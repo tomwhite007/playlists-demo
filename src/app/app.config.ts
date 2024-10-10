113c82f2f661ac9de09030a69ae48c52d690fc8f
Tom White <tomwhite007@gmail.com>
2024-10-10 14:33:42 +0100

    feat: add effects

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,8 @@ import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideStore } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideStore(reducers, { metaReducers })]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), provideStore(reducers, { metaReducers }), provideEffects()]
 };
