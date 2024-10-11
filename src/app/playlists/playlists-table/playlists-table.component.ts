d6899ecead93370e35a2a528020652bc6adc214a
Tom White <tomwhite007@gmail.com>
2024-10-11 10:50:16 +0100

    feat: generate table component

---
+++ src/app/playlists/playlists-table/playlists-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-playlists-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './playlists-table.component.html',
+  styleUrl: './playlists-table.component.scss'
+})
+export class PlaylistsTableComponent {
+
+}
